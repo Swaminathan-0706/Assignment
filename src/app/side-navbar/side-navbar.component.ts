21ceaa26a2c2ba71553f3aa40d8f9e0edbf8976d
Swaminathan <sayeeiyer@gmail.com>
2020-08-24 00:43:02 +0530

    Side bar-Second Commit

--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
+
+import { AppService } from '../app.service';
+import { MENUS } from '../Utilities/constants';
 
 @Component({
   selector: 'app-side-navbar',
   templateUrl: './side-navbar.component.html',
   styleUrls: ['./side-navbar.component.scss']
 })
-export class SideNavbarComponent implements OnInit {
+export class SideNavbarComponent  {
 
-  constructor() { }
+  title = 'employee-app';
 
-  ngOnInit(): void {
+  constructor(public appService: AppService,
+    private route: ActivatedRoute,
+    private router: Router) {}
+
+  menuClick(menu) {
+    switch(menu) {
+      case MENUS.EMPLOYEES_MENU:
+        this.navigateToEmployees();
+        break;
+      case MENUS.LOGOUT_MENU:
+        this.logout();
+        break;
+      default:
+        break;
+    }
+  }
+
+  navigateToEmployees() {
+    this.router.navigate(['employees'], { relativeTo: this.route });
+  }
+
+  logout() {
+    this.appService.setIsLoggedIn(false);
+    this.router.navigate([''], { relativeTo: this.route });
   }
 
 }
